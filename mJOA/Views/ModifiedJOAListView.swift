0ca82e37ce527f0ae0b6ced8c12ebbd1ec700221
classyoon <96508736+classyoon@users.noreply.github.com>
2024-05-24 19:02:04 -0400

    Initial Commit

---
+++ mJOA/Views/ModifiedJOAListView.swift
@@ -0,0 +1,64 @@
+//
+//  ModifiedJOAListView.swift
+//  mJOA
+//
+//  Created by Tim Yoon on 5/23/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ModifiedJOAListView: View {
+    @Environment(\.modelContext) private var modelContext//Where is the model context, in the enviroment. Important. Think of it as in RAM that interfaces to the persistent data store.
+    @Query private var joaList: [ModifiedJOA] //This how you get the data out of swift data. It may look like an array, but its actually an ordered list.
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(joaList) { joa in
+                    NavigationLink {
+                        ModifiedJOAView(mJOA: joa)
+                    } label: {
+                        VStack {
+                            Text("\(joa.timestamp)")
+                            Text("\(joa.scoreText)")
+                        }
+                    }
+                }.onDelete(perform: deleteItems)
+
+            }
+            .navigationTitle("JOAs")
+            .toolbar {
+                EditButton()
+                Button(action: {
+                    addItem()
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+        }
+    }
+    private func addItem() {
+        withAnimation {
+            let newItem = ModifiedJOA()
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(joaList[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: ModifiedJOA.self, configurations: config)
+    
+    
+    return ModifiedJOAListView()
+        .modelContainer(container)
+}
